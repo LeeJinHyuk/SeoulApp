abd2af6e0be1aff499ee7ab367032c05b5aef86b
LeeJinHyuk <eerto@naver.com>
2016-07-31 23:38:06 +0900

    webpack css loader 설정 추가 - css 파일도 bundle에 포함되도록 설정 추가

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/**
+ * Created by LeeJinHyuk on 2016-07-31.
+ */
+var webpack = require("webpack");
+
+var config = {
+    entry : {
+      main : __dirname + "/scripts/main.js"
+    },
+    output : {
+        path : __dirname + "/scripts",
+        filename : "[name].bundle.js",
+        chunkFilename : "[id].bundle.js"
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.js$/,
+                loader : "babel",
+                query : {
+                    presets : ["es2015", "react"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    }
+};
+
+module.exports = config;
