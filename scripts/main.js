0693b637f1689910bbfbe212cd49604701176b4f
eerto <jinhyuk.lee@traport.com>
2016-08-25 16:39:44 +0900

    - webpack 설정 후 컴포넌트 테스트 돔 트리에 컴포넌트 마운트 테스트

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,10 @@
  * Created by LeeJinHyuk on 2016-07-31.
  */
 "use strict";
+import React from "react";
+import ReactDom from "react-dom";
+import Container from "./view/container";
 
 window.onload = function() {
-
+  ReactDom.render(<Container />, document.getElementById("container"));
 };
--- scripts/view/container.js
+++ scripts/view/container.js
@@ -1,3 +1,17 @@
 /**
  * Created by eerto_000 on 2016-08-03.
  */
+import React from "react";
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default Container;
