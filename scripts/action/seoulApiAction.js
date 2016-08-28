87e890cc0a099d0022cbf384e81e08d981d9334a
eerto <eerto@naver.com>
2016-08-28 22:07:46 +0900

    - action & store 수정 기본 컴포넌트 내용 추가

--- scripts/action/seoulApiAction.js
+++ scripts/action/seoulApiAction.js
@@ -1,3 +1,10 @@
 /**
  * Created by eerto_000 on 2016-08-28.
  */
+import Reflux from 'reflux';
+
+let SeoulApiAction = Reflux.createActions([
+    "getDayCareCenterInformation"
+]);
+
+export default SeoulApiAction;
--- scripts/store/seoulApiStore.js
+++ scripts/store/seoulApiStore.js
@@ -1,3 +1,21 @@
 /**
  * Created by eerto_000 on 2016-08-28.
  */
+import Reflux from "reflux";
+import SeoulApiAction from "../action/seoulApiAction";
+
+let SeoulApiStore = Reflux.createStore({
+    listenables : [SeoulApiAction],
+
+    init : function() {
+        // 초기화
+
+    },
+    onGetDayCareCenterInformation(postData) {
+
+    }
+
+});
+
+export default SeoulApiStore;
+
