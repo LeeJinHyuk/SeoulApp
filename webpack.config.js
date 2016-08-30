a3c701bc3446dfb6f8c7c435ac2002c99e9d4c27
eerto <eerto@naver.com>
2016-08-31 00:23:57 +0900

    - 모듈 설치 및 webpack 수정 babel 관려 모듈 및 preset, plugins 추가 babel 6.x 버전에서 @ 표시와 mixin decorate 사용을 위해 모듈 추가와 webpack을 수정함. 향후 내용 다시 확인 필요

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ var config = {
                 test : /\.js$/,
                 loader : "babel",
                 query : {
-                    presets : ["es2015", "react"]
+                    presets : ["es2015", "react", "stage-0"],
+                    plugins: ['transform-decorators-legacy']
                 }
             },
             {
