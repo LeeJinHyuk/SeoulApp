524c1e594a646c3bc30c9719588f042804c85aad
eerto <eerto@naver.com>
2016-09-04 23:25:23 +0900

    - jquery 모듈 추가 및 데이터 요청 부분 수정 jquery ajax 요청 부분 코드 추가 공공데이터 인증 키 및 데이터 자체에 문제가 있어 보임...

--- scripts/store/seoulApiStore.js
+++ scripts/store/seoulApiStore.js
@@ -2,8 +2,7 @@
  * Created by eerto_000 on 2016-08-28.
  */
 import Reflux from "reflux";
-import Superagent from "superagent";
-import SuperagentJsonp from "superagent-jsonp";
+import $ from "jquery";
 import SeoulApiAction from "../action/seoulApiAction";
 
 let SeoulApiStore = Reflux.createStore({
@@ -11,9 +10,17 @@ let SeoulApiStore = Reflux.createStore({
 
     init : function() {
         // 초기화
+        // this.data = {
+        //     query : "param=ServiceKey=HF1eUr96KfQkuZe3Pl1v0stWJvCU8eH72E%252BPGfe%252BiUOMDUlk0P1%252FMgO4SpXf0qq74hzOF7ctuBDJl2L7aXXOsw%253D%253D" +
+        //     "%26s_page=1%26s_list=1%26type=json%26numOfRows=200%26pageNo=1"
+        // };
         this.data = {
-            query : "param=ServiceKey=HF1eUr96KfQkuZe3Pl1v0stWJvCU8eH72E%252BPGfe%252BiUOMDUlk0P1%252FMgO4SpXf0qq74hzOF7ctuBDJl2L7aXXOsw%253D%253D" +
-            "%26s_page=1%26s_list=1%26type=json%26numOfRows=200%26pageNo=1"
+            serviceKey : "HF1eUr96KfQkuZe3Pl1v0stWJvCU8eH72E%2BPGfe%2BiUOMDUlk0P1%2FMgO4SpXf0qq74hzOF7ctuBDJl2L7aXXOsw%3D%3D",
+            s_page : encodeURIComponent("1"),
+            s_list : encodeURIComponent("100"),
+            numOfRows : encodeURIComponent("200"),
+            pageNo : encodeURIComponent("1"),
+            type : "json"
         };
 
 
@@ -31,15 +38,32 @@ let SeoulApiStore = Reflux.createStore({
     },
     onGetDayCareCenterInformation() {
         console.log("[SeoulApiStore] onGetDayCareCenterInformation");
-        Superagent.get("http://api.data.go.kr/openapi/379bee0d-9545-4436-b039-53c5444da34f")
-            .use(SuperagentJsonp({
-                timeout : 3000
-            }))
-            .query(this.data.query)
-            .end(function(err, res){
-                console.log(err);
-                console.log(res);
-            });
+
+        $.ajax({
+            url: "http://api.data.go.kr/openapi/379bee0d-9545-4436-b039-53c5444da34f",
+            dataType : "jsonp",
+            method : "GET",
+            data : this.data,
+            jsonp : false,
+            jsonpCallback: "jsonCallback",
+            cache : true,
+            success : function(data, text, xhr) {
+                console.log("success");
+            },
+            error : function (xhr, text, error) {
+                console.log("error");
+            }
+        });
+
+        // Superagent.get("http://api.data.go.kr/openapi/379bee0d-9545-4436-b039-53c5444da34f")
+        //     .use(SuperagentJsonp({
+        //         timeout : 3000
+        //     }))
+        //     .query(this.data.query)
+        //     .end(function(err, res){
+        //         console.log(err);
+        //         console.log(res);
+        //     });
 
 
         // Superagent.get("http://api.data.go.kr/openapi/379bee0d-9545-4436-b039-53c5444da34f")
