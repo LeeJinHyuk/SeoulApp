319ae1202077820f085102acd0ae227f697ecc4e
eerto <eerto@naver.com>
2016-09-25 22:54:01 +0900

    - 전역으로 사용할 고정 데이터 모듈 추가 - navi.css 파일 추가

--- scripts/globalData.js
+++ scripts/globalData.js
@@ -1,3 +1,16 @@
 /**
  * Created by eerto_000 on 2016-09-25.
  */
+const GD = {
+    TITLE : {
+        JOBFAIR : "취업설명회",
+        EMPLOYMENT_NOTICE : "채용공고"
+    },
+    NAVITYPE : {
+        JOBFAIR: 0, // 취업설명회 타입
+        EMPLOYMENT_NOTICE: 1, // 채용공고 타입
+        DETAIL: 2 // 상세화면 타입
+    }
+}
+
+export default GD;
