e1326eb3b382b48a964428f8cfd1fdd320be8e3e
eerto <eerto@naver.com>
2016-09-28 00:26:23 +0900

    모듈 추가 및 webpack 설정 수정 - less 및 less-loader 모듈 추가 - less 설정을 위한 webpack 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ var config = {
             {
                 test: /\.css$/,
                 loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
             }
         ]
     }
