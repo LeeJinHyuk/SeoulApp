0b3ca36cb3e8fdad961637790826a5fed71a5bb0
eerto <eerto@naver.com>
2016-10-02 20:06:58 +0900

    공공 데이터 api 요청 로직 수정 - 이미 요청한 데이터가 로컬 스토리지에 존재할 경우 재요청 하지 않도록 수정

--- scripts/store/seoulApiStore.js
+++ scripts/store/seoulApiStore.js
@@ -3,18 +3,21 @@
  */
 import Reflux from "reflux";
 import $ from "jquery";
+import GD from "../globalData";
 import SeoulApiAction from "../action/seoulApiAction";
 
 let SeoulApiStore = Reflux.createStore({
     listenables : [SeoulApiAction],
 
     init : function() {
+        let tmpJobFairData = localStorage.getItem(GD.STORAGE_KEY.JOBFAIR);
+
         this.TYPE = {
             JOBFIARLIST : "jobFiarList",
             RECURITLIST : "recuritList",
             RECURITDETAIL : "recuritDetail"
         };
-        this.data = {
+        this.API = {
             jobFairUrl :
                 "http://openapi.seoul.go.kr:8088/4150495f3231303670726f6f636f737330/json/JobFairInfo/1/999/",
             recruitmentListUrl :
@@ -23,6 +26,9 @@ let SeoulApiStore = Reflux.createStore({
                 "&numOfRows=999&pageNo=1",
             recruitmentDetailUrl :
                 "http://openapi.mpm.go.kr/openapi/service/RetrievePblinsttEmpmnInfoService/getItem"
+        };
+        this.list = {
+            jobFair : tmpJobFairData !== undefined ? JSON.parse(tmpJobFairData) : undefined // 채용설명회 리스트 데이터
         };
     },
     ajaxFactory : function(url, dataType) {
@@ -34,7 +40,12 @@ let SeoulApiStore = Reflux.createStore({
             cache : true,
             success : function(data, text, xhr) {
                 console.log("[SeoulApiStore] success");
-                that.trigger(data, that.TYPE.JOBFIARLIST, that.TYPE);
+                switch(url) {
+                    case that.API.jobFairUrl:
+                        localStorage.setItem(GD.STORAGE_KEY.JOBFAIR, JSON.stringify(data));
+                        that.trigger(data, that.TYPE.JOBFIARLIST, that.TYPE);
+                        break;
+                }
             },
             error : function (xhr, text, error) {
                 console.log("[SeoulApiStore] error");
@@ -44,19 +55,27 @@ let SeoulApiStore = Reflux.createStore({
     onGetJobFairList() {
         // 취업 설명회 리스트 요청 API
         console.log("[SeoulApiStore] onGetJobFairList");
-        this.ajaxFactory(this.data.jobFairUrl, "jsonp");
+        if (this.list.jobFair) {
+            // 이미 받아온 데이터가 존재할 경우 로컬스토리지 데이터 사용
+            console.log("[SeoulApiStore] onGetJobFairList local data");
+            this.trigger(this.list.jobFair, this.TYPE.JOBFIARLIST, this.TYPE);
+        } else {
+            // 없는 경우 서버에서 데이터 요청
+            console.log("[SeoulApiStore] onGetJobFairList server data");
+            this.ajaxFactory(this.API.jobFairUrl, "jsonp");
+        }
     },
     onGetrecuritmentList() {
         // TODO 취업공고 리스트 요청 방법 확인 필요(xml cors)
         console.log("[SeoulApiStore] onGetrecuritmentList");
-        this.ajaxFactory(this.data.recruitmentListUrl, "jsonp");
+        this.ajaxFactory(this.API.recruitmentListUrl, "jsonp");
     },
     onGetrecuritDetail(idx) {
         // TODO 취업공고 상세정보 요청 방법 확인 필요(xml cors)
         console.log("[SeoulApiStore] onGetrecuritDetail");
         let requestUrl;
         
-        requestUrl = this.data.recruitmentDetailUrl + "?idx=" + idx + 
+        requestUrl = this.API.recruitmentDetailUrl + "?idx=" + idx +
                 "&ServiceKey=HF1eUr96KfQkuZe3Pl1v0stWJvCU8eH72E%2BPGfe%2BiUOMDUlk0P1%2FMgO4SpXf0qq74hzOF7ctuBDJl2L7aXXOsw%3D%3D" + 
             "&numOfRows=999&pageNo=1";
         this.ajaxFactory(requestUrl, "xml");
