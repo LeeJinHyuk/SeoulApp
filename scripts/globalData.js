deda2cfdd660225f7e58dbf16ba304e1830d475c
eerto <eerto@naver.com>
2016-10-02 20:06:24 +0900

    전역 데이터 및 네비게이션 스타일 수정 - 전역 데이터에 로컬 스토리지 키 추가 - 네비게이션 padding 값 조절

--- scripts/globalData.js
+++ scripts/globalData.js
@@ -10,6 +10,9 @@ const GD = {
         JOBFAIR: 0, // 취업설명회 타입
         EMPLOYMENT_NOTICE: 1, // 채용공고 타입
         DETAIL: 2 // 상세화면 타입
+    },
+    STORAGE_KEY : {
+        JOBFAIR : "jobfair"
     }
 }
 
