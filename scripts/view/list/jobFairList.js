0afeae2ce4f8fd8cccf95d5a77374d945d903e92
eerto <eerto@naver.com>
2016-10-05 00:07:34 +0900

    설명회 정렬 선택 팝업 로직 및 스타일 추가 - 전체 또는 현재 년도로 정렬 가능하도록 로직 추가 - 선택된 정렬 기준을 볼 수 있도록 스타일 수정

--- scripts/view/list/jobFairList.js
+++ scripts/view/list/jobFairList.js
@@ -23,7 +23,11 @@ class JobFairList extends React.Component {
         // 리스트 아이템 생성
         this.makeItem = this.makeItem.bind(this);
         // 리스트 모드 변경
-        this.changeCondition = this.changeCondition.bind(this);
+        this.openConditionPopup = this.openConditionPopup.bind(this);
+        // 하단 리스트 모드 변경 팝업의 스타일 설정
+        this.setSeletedCondition = this.setSeletedCondition.bind(this);
+        // 리스트 모드 변경 팝업에서 선택
+        this.selectCondition = this.selectCondition.bind(this);
     };
 
     // componentWillMount() {
@@ -114,10 +118,74 @@ class JobFairList extends React.Component {
         return itemTag;
     };
 
-    changeCondition(e) {
+    openConditionPopup(e) {
         this.setState({
             conditionMode : true
         });
+    };
+
+    setSeletedCondition(mode) {
+        if (mode === GD.JOBLISTMODE.TOTAL) {
+            if (this.state.listMode === GD.JOBLISTMODE.TOTAL) {
+                return "activate";
+            } else {
+                return "";
+            }
+        } else {
+            if (this.state.listMode === GD.JOBLISTMODE.CURRENT_YEAR) {
+                return "activate";
+            } else {
+                return "";
+            }
+        }
+    };
+    selectCondition(e) {
+        let tmpListData = [];
+        let currentYear;
+
+        if (e.target.className.indexOf("conditionPopup") > -1) {
+            // 배경 선택 시 이벤트. condition popup 만 닫힌다.
+            this.setState({
+                conditionMode : false
+            });
+        } else if (e.target.className.indexOf("total") > -1) {
+            // 전체 기간 선택 시 이벤트.
+            if (this.state.listMode === GD.JOBLISTMODE.TOTAL) {
+                this.setState({
+                    conditionMode : false
+                });
+            } else {
+                this.setState({
+                    conditionMode : false,
+                    listData : this.props.listData,
+                    listMode : GD.JOBLISTMODE.TOTAL
+                });
+            }
+        } else {
+            // 현재 년도 선택 시 이벤트.
+            e.stopPropagation();
+            if (this.state.listMode === GD.JOBLISTMODE.CURRENT_YEAR) {
+                this.setState({
+                    conditionMode : false
+                });
+            } else {
+                currentYear = new Date().getFullYear().toString();
+
+                tmpListData = this.props.listData.filter(function(item, index, array) {
+                    if (currentYear === item.JOBFAIR_YEAR) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+
+                this.setState({
+                    conditionMode : false,
+                    listData : tmpListData,
+                    listMode : GD.JOBLISTMODE.CURRENT_YEAR
+                });
+            }
+        }
     };
 
     render() {
@@ -125,7 +193,7 @@ class JobFairList extends React.Component {
             this.state.listData !== undefined
                 ?
                 <div className="jobFairList">
-                    <div className="conditionTab" onClick={this.changeCondition}>
+                    <div className="conditionTab" onClick={this.openConditionPopup}>
                         {
                             this.state.listMode === GD.JOBLISTMODE.TOTAL
                                 ?
@@ -138,10 +206,20 @@ class JobFairList extends React.Component {
                     {
                         this.state.conditionMode === true
                             ?
-                            <div className="conditionPopup">
+                            <div
+                                className="conditionPopup"
+                                onClick={this.selectCondition}>
                                 <ul>
-                                    <li>전체기간</li>
-                                    <li>{new Date().getFullYear().toString()}</li>
+                                    <li
+                                        className={"total " + this.setSeletedCondition(GD.JOBLISTMODE.TOTAL)}
+                                        onClick={this.selectCondition}>
+                                        전체기간
+                                    </li>
+                                    <li
+                                        className={"current " + this.setSeletedCondition(GD.JOBLISTMODE.CURRENT_YEAR)}
+                                        onClick={this.selectCondition}>
+                                        {new Date().getFullYear().toString()}
+                                    </li>
                                 </ul>
                             </div>
                             :
