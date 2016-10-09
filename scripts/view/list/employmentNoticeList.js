2cb33f7331066cf1c67d891db21963198fc9ae17
eerto <eerto@naver.com>
2016-10-09 22:36:35 +0900

    공고 리스트 상단 탭 스타일 수정 및 검색창 노출 로직 추가 - 상단 탭 노출되도록 스타일 수정 - 검색조건 창 노출되도록 state 로직 추가

--- scripts/view/list/employmentNoticeList.js
+++ scripts/view/list/employmentNoticeList.js
@@ -13,13 +13,16 @@ class EmploymentNoticeList extends React.Component {
         this.state = {
             listMode : GD.EMPLOYMENTNOTICELISTMODE.NORMAL,
             listData : undefined,
-            maxPrintData : 20
+            maxPrintData : 20,
+            isPrintSearchTab : false
         };
 
         // shouldComponentUpdate 성능 향상 모듈 실행
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         // 리스트 아이템 생성
         this.makeItem = this.makeItem.bind(this);
+        // 검색 조건 팝업 노출
+        this.openConditionPopup = this.openConditionPopup.bind(this);
         // 테스트 파싱 함수1
         this.testCall = this.testCall.bind(this);
         // 테스트 파싱 함수2
@@ -66,6 +69,12 @@ class EmploymentNoticeList extends React.Component {
     //
     // };
 
+    openConditionPopup(e) {
+        this.setState({
+            isPrintSearchTab : true
+        });
+    };
+
     testCall(nextProps) {
         // ACDMCR_NM 학력코드명
         // EMPLYM_STLE_CMMN_MM 고용형태코드명
@@ -75,6 +84,7 @@ class EmploymentNoticeList extends React.Component {
         // JO_FEINSR_SBSCRB_NM 4대보험
         // MODEL_MTH_NM 전형방법
         // RCEPT_MTH_NM 접수방법
+        // MNGR_INSTT_NM 지역구
 
         let ACDMCR_NM = [];
         let EMPLYM_STLE_CMMN_MM = [];
@@ -84,6 +94,7 @@ class EmploymentNoticeList extends React.Component {
         let JO_FEINSR_SBSCRB_NM = [];
         let MODEL_MTH_NM = [];
         let RCEPT_MTH_NM = [];
+        let MNGR_INSTT_NM = [];
 
         let that = this;
         nextProps.listData.map((item, i) => {
@@ -96,6 +107,7 @@ class EmploymentNoticeList extends React.Component {
                 JO_FEINSR_SBSCRB_NM.push(item.ACDMCR_NM);
                 MODEL_MTH_NM.push(item.ACDMCR_NM);
                 RCEPT_MTH_NM.push(item.ACDMCR_NM);
+                MNGR_INSTT_NM.push(item.MNGR_INSTT_NM);
             } else {
                 that.testCallIner(ACDMCR_NM, item.ACDMCR_NM);
                 that.testCallIner(EMPLYM_STLE_CMMN_MM, item.EMPLYM_STLE_CMMN_MM);
@@ -105,6 +117,7 @@ class EmploymentNoticeList extends React.Component {
                 that.testCallIner(JO_FEINSR_SBSCRB_NM, item.JO_FEINSR_SBSCRB_NM);
                 that.testCallIner(MODEL_MTH_NM, item.MODEL_MTH_NM);
                 that.testCallIner(RCEPT_MTH_NM, item.RCEPT_MTH_NM);
+                that.testCallIner(MNGR_INSTT_NM, item.MNGR_INSTT_NM);
             }
         });
         console.log("ACDMCR_NM(학력코드명) : " + JSON.stringify(ACDMCR_NM));
@@ -115,6 +128,7 @@ class EmploymentNoticeList extends React.Component {
         console.log("JO_FEINSR_SBSCRB_NM(4대보험) : " + JSON.stringify(JO_FEINSR_SBSCRB_NM));
         console.log("MODEL_MTH_NM(전형방법) : " + JSON.stringify(MODEL_MTH_NM));
         console.log("RCEPT_MTH_NM(접수방법) : " + JSON.stringify(RCEPT_MTH_NM));
+        console.log("MNGR_INSTT_NM(지역구) : " + JSON.stringify(MNGR_INSTT_NM));
     };
 
     testCallIner(tmpArray, data) {
@@ -173,7 +187,7 @@ class EmploymentNoticeList extends React.Component {
                         </div>
                     </div>
                     {
-                        this.state.listMode === GD.EMPLOYMENTNOTICELISTMODE.SEARCH 
+                        this.state.listMode === GD.EMPLOYMENTNOTICELISTMODE.SEARCH
                             ?
                             <div className="bottomTab">
                                 <span>test입니다</span>
@@ -182,6 +196,33 @@ class EmploymentNoticeList extends React.Component {
                             null
                     }
                     {this.makeItem()}
+                    {
+                        this.state.isPrintSearchTab === true
+                            ?
+                            <div
+                                className="conditionPopup">
+                                <div className="region">
+                                    <div className="selectRegion">
+                                        <span>지역</span>
+                                        <select id="soflow">
+                                            <option>지역선택</option>
+                                            <option>강남구</option>
+                                            <option>도봉구</option>
+                                        </select>
+                                        <span>최대 3개까지 선택가능</span>
+                                    </div>
+                                    <div className="selectedRegion">
+                                        <ul>
+                                            <li>test1</li>
+                                            <li>test2</li>
+                                            <li>test3</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            :
+                            null
+                    }
                 </div>
                 :
                 null
