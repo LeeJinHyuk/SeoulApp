d6773ed042693baa8a616a961100bcfd63e4809f
eerto <eerto@naver.com>
2016-10-10 23:14:28 +0900

    지역 선택 전역 변수 추가 - 지역명 리스트 변수 추가

--- scripts/globalData.js
+++ scripts/globalData.js
@@ -27,7 +27,34 @@ const GD = {
     APICALL_TYPE : {
         START : 0, // 시작 시 API CALL
         TAB : 1 // TAB 으로 API CALL
-    }
+    },
+    REGION : [
+        {NAME : "강남구", CODE : "01"},
+        {NAME : "강동구", CODE : "02"},
+        {NAME : "강북구", CODE : "03"},
+        {NAME : "강서구", CODE : "04"},
+        {NAME : "구로구", CODE : "05"},
+        {NAME : "금천구", CODE : "06"},
+        {NAME : "관악구", CODE : "07"},
+        {NAME : "광진구", CODE : "08"},
+        {NAME : "노원구", CODE : "09"},
+        {NAME : "도봉구", CODE : "10"},
+        {NAME : "동대문구", CODE : "11"},
+        {NAME : "동작구", CODE : "12"},
+        {NAME : "마포구", CODE : "13"},
+        {NAME : "서대문구", CODE : "14"},
+        {NAME : "서초구", CODE : "15"},
+        {NAME : "성동구", CODE : "16"},
+        {NAME : "성북구", CODE : "17"},
+        {NAME : "송파구", CODE : "18"},
+        {NAME : "은평구", CODE : "19"},
+        {NAME : "양천구", CODE : "20"},
+        {NAME : "영등포구", CODE : "21"},
+        {NAME : "용산구", CODE : "22"},
+        {NAME : "종로구", CODE : "23"},
+        {NAME : "중랑구", CODE : "24"},
+        {NAME : "중구", CODE : "25"}
+    ]
 }
 
 export default GD;
