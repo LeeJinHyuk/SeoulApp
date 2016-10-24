66af1815af131229041259c87931efe0048918b8
eerto <eerto@naver.com>
2016-10-24 22:53:09 +0900

    사용하지 않는 테스트 코드 제거 - 테스트 코드 제거

--- scripts/view/list/employmentNoticeList.js
+++ scripts/view/list/employmentNoticeList.js
@@ -37,10 +37,6 @@ class EmploymentNoticeList extends React.Component {
         this.selectCondition = this.selectCondition.bind(this);
         // 더보기 버튼 선택시 실행 콜백
         this.loadAdditionalData = this.loadAdditionalData.bind(this);
-        // 테스트 파싱 함수1
-        this.testCall = this.testCall.bind(this);
-        // 테스트 파싱 함수2
-        this.testCallIner = this.testCallIner.bind(this);
     };
 
     componentWillMount() {
@@ -57,7 +53,6 @@ class EmploymentNoticeList extends React.Component {
 
     componentWillReceiveProps(nextProps) {
         console.log("[EmploymentNoticeList] componentWillReceiveProps");
-        this.testCall(nextProps);
         this.setState({
             listData : nextProps.listData
         });
@@ -160,75 +155,6 @@ class EmploymentNoticeList extends React.Component {
             this.setState({
                 maxPrintData : this.state.maxPrintData + 20
             });
-        }
-    };
-
-    testCall(nextProps) {
-        // ACDMCR_NM 학력코드명
-        // EMPLYM_STLE_CMMN_MM 고용형태코드명
-        // CAREER_CND_NM 경력조건코드명
-        // RET_GRANTS_NM 퇴직금구분
-        // WORK_TM_NM 근무형태
-        // JO_FEINSR_SBSCRB_NM 4대보험
-        // MODEL_MTH_NM 전형방법
-        // RCEPT_MTH_NM 접수방법
-        // MNGR_INSTT_NM 지역구
-
-        let ACDMCR_NM = [];
-        let EMPLYM_STLE_CMMN_MM = [];
-        let CAREER_CND_NM = [];
-        let RET_GRANTS_NM = [];
-        let WORK_TM_NM = [];
-        let JO_FEINSR_SBSCRB_NM = [];
-        let MODEL_MTH_NM = [];
-        let RCEPT_MTH_NM = [];
-        let MNGR_INSTT_NM = [];
-
-        let that = this;
-        nextProps.listData.map((item, i) => {
-            if (ACDMCR_NM.length === 0) {
-                ACDMCR_NM.push(item.ACDMCR_NM);
-                EMPLYM_STLE_CMMN_MM.push(item.ACDMCR_NM);
-                CAREER_CND_NM.push(item.ACDMCR_NM);
-                RET_GRANTS_NM.push(item.ACDMCR_NM);
-                WORK_TM_NM.push(item.ACDMCR_NM);
-                JO_FEINSR_SBSCRB_NM.push(item.ACDMCR_NM);
-                MODEL_MTH_NM.push(item.ACDMCR_NM);
-                RCEPT_MTH_NM.push(item.ACDMCR_NM);
-                MNGR_INSTT_NM.push(item.MNGR_INSTT_NM);
-            } else {
-                that.testCallIner(ACDMCR_NM, item.ACDMCR_NM);
-                that.testCallIner(EMPLYM_STLE_CMMN_MM, item.EMPLYM_STLE_CMMN_MM);
-                that.testCallIner(CAREER_CND_NM, item.CAREER_CND_NM);
-                that.testCallIner(RET_GRANTS_NM, item.RET_GRANTS_NM);
-                that.testCallIner(WORK_TM_NM, item.WORK_TM_NM);
-                that.testCallIner(JO_FEINSR_SBSCRB_NM, item.JO_FEINSR_SBSCRB_NM);
-                that.testCallIner(MODEL_MTH_NM, item.MODEL_MTH_NM);
-                that.testCallIner(RCEPT_MTH_NM, item.RCEPT_MTH_NM);
-                that.testCallIner(MNGR_INSTT_NM, item.MNGR_INSTT_NM);
-            }
-        });
-        console.log("ACDMCR_NM(학력코드명) : " + JSON.stringify(ACDMCR_NM));
-        console.log("EMPLYM_STLE_CMMN_MM(고용형태코드명) : " + JSON.stringify(EMPLYM_STLE_CMMN_MM));
-        console.log("CAREER_CND_NM(경력조건코드명) : " + JSON.stringify(CAREER_CND_NM));
-        console.log("RET_GRANTS_NM(퇴직금구분) : " + JSON.stringify(RET_GRANTS_NM));
-        console.log("WORK_TM_NM(근무형태) : " + JSON.stringify(WORK_TM_NM));
-        console.log("JO_FEINSR_SBSCRB_NM(4대보험) : " + JSON.stringify(JO_FEINSR_SBSCRB_NM));
-        console.log("MODEL_MTH_NM(전형방법) : " + JSON.stringify(MODEL_MTH_NM));
-        console.log("RCEPT_MTH_NM(접수방법) : " + JSON.stringify(RCEPT_MTH_NM));
-        console.log("MNGR_INSTT_NM(지역구) : " + JSON.stringify(MNGR_INSTT_NM));
-    };
-
-    testCallIner(tmpArray, data) {
-        let flag = false;
-
-        for (let i = 0; i < tmpArray.length; i++) {
-            if (tmpArray[i] === data) {
-                flag = true;
-            }
-        }
-        if (flag === false) {
-            tmpArray.push(data);
         }
     };
 
