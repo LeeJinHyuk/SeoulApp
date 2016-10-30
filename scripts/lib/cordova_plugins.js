64275a831999c8859943435a11db8aac113074f8
eerto <eerto@naver.com>
2016-10-30 15:41:24 +0900

    cordova plugin 추가 - splashScreen plugin 추가

--- scripts/lib/cordova_plugins.js
+++ scripts/lib/cordova_plugins.js
@@ -8,13 +8,31 @@ module.exports = [
             "navigator.notification",
             "navigator"
         ]
+    },
+    {
+        "id": "cordova-plugin-backbutton.Backbutton",
+        "file": "plugins/cordova-plugin-backbutton/www/Backbutton.js",
+        "pluginId": "cordova-plugin-backbutton",
+        "clobbers": [
+            "navigator.Backbutton"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.0",
-    "cordova-plugin-toaster": "0.0.1"
+    "cordova-plugin-toaster": "0.0.1",
+    "cordova-plugin-backbutton": "0.3.0",
+    "cordova-plugin-splashscreen": "4.0.0"
 };
 // BOTTOM OF METADATA
 });
